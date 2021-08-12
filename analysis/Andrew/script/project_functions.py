185419c0d7accaee5b7ccf9b2687cf8051f50d92
daihuyang <daihuyang@gmail.com>
2021-08-11 23:24:32 -0700

    adding load and process and wrangle def

--- analysis/Andrew/script/project_functions.py
+++ analysis/Andrew/script/project_functions.py
@@ -10,7 +10,18 @@ def clean_data(path):
     data_clean = data_clean[data_clean['Year'] >= 1985]
     data_clean = data_clean[data_clean['Year']<=2015]
     return data_clean
-def load_process(){
-    
-}
+def load_process(data,year):
+    data=data[data['Year']==year]
+    state_list=list(range(1,max(data['State'])+1))
+    state_list
+    column_names=['State','percenatge']
+    df=pd.DataFrame(columns=column_names)
+    df['State']=state_list
+    Statebirth=[]
+    df
 
+def wrangle(old_data,new_data,new_list,newlist_name):
+    for item in new_data['State']:
+        new_list.append(old_data.loc[old_data['State']==item,'countyBirths'].sum())
+
+    new_data[newlist_name]=new_list
